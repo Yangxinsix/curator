8f98684d3a9d1c6967781b560a6c59b0e78d724d
Xin Yang <xinyang@dtu.dk>
2024-08-05 16:31:35 +0800

    trajectory read bug

--- curator/data/utils.py
+++ curator/data/utils.py
@@ -11,6 +11,7 @@ from ase import Atoms
 from pathlib import PosixPath
 from ase.io.trajectory import TrajectoryReader
 from omegaconf import ListConfig
+import os
 
 def read_trajectory(ase_db):
     if isinstance(ase_db, (str, PosixPath)):
@@ -23,11 +24,11 @@ def read_trajectory(ase_db):
         if all(isinstance(item, Atoms) for item in ase_db):
             db = ase_db
         elif all(isinstance(item, (str, PosixPath)) and str(item).endswith('.traj') for item in ase_db):
-            db = Trajectory([str(item) for item in ase_db])
+            db = Trajectory([str(item) for item in ase_db if not os.path.getsize(item)])
         else:
             db = []
             for item in ase_db:
-                if isinstance(item, (str, PosixPath)):
+                if isinstance(item, (str, PosixPath)) and not os.path.getsize(item):
                     item = str(item)  # Convert PosixPath to string if necessary
                     db += read(item, ':')
     elif isinstance(ase_db, TrajectoryReader):
