5e7e53919804d75098948b178d695ef36558b865
Xin Yang <xinyang@dtu.dk>
2024-10-31 11:39:39 +0800

    virial read

--- curator/data/_data_reader.py
+++ curator/data/_data_reader.py
@@ -161,4 +161,7 @@ class AseDataReader(DataReader):
         except (AttributeError, RuntimeError):
             pass
         
+        if atoms.info.get('virial'):
+            atoms_data[properties.virial] = atoms.info.get('virial')[:, [0, 4, 8, 5, 2, 1]]
+        
         return atoms_data
