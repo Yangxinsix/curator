764bae6d18c06113478d33d87fc13cdc3fe65474
Xin Yang <xinyang@dtu.dk>
2025-07-11 14:58:33 +0800

    delete

--- build/lib/curator/__init__.py
+++
@@ -1,2 +0,0 @@
-import os
-os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
--- build/lib/curator/model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .painn import Painn
-from .nequip import Nequip
-from .mace import MACE
-from .base import (
-    NeuralNetworkPotential,
-    LitNNP,
-)
-from .ensemble import EnsembleModel
