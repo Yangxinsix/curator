36aec1df3e50a9a96b5abd136a3fd7b3450f74f4
Xin Yang <xinyang@dtu.dk>
2025-11-11 20:17:22 +0800

    torch version compatiability

--- curator/__init__.py
+++ curator/__init__.py
@@ -1,2 +1,15 @@
+from curator import (
+    data,
+    layer,
+    model,
+    select,
+    simulate,
+    label,
+)
+
 import os
 os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
+
+import torch
+# ensure delete always succeeds
+torch._utils._thread_local_state.map_location = None
