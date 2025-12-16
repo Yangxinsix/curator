d515bb8ff45bc5e2e2612daa2a444cb959947d86
Xin Yang <xinyang@dtu.dk>
2025-12-16 14:19:09 +0800

    init

--- curator/__init__.py
+++ curator/__init__.py
@@ -1,8 +1,12 @@
-from curator import (
-    data,
-    layer,
-    model,
-    select,
-    simulate,
-    label,
-)
+import torch
+
+# parsing torch version for no weight loading
+ver = torch.__version__.split("+")[0]
+major, minor, *_ = map(int, ver.split("."))
+if (major > 2) or (major == 2 and minor > 4):
+    import os
+    os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
+
+from . import data, layer, model, select, simulate, label
+
+__all__ = ["data", "layer", "model", "select", "simulate", "label"]
